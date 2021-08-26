784cb02e81bc45236edef6a58e7d54196cbbd6b5
MedhaRaj <59068494+MedhaRaj@users.noreply.github.com>
2021-08-26 13:06:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var i = 0,
     images = [
-       "https://i.ibb.co/tKy3h5Z/1.png",
-"https://i.ibb.co/tsZZkbR/2.png",
-"https://i.ibb.co/B3f2HJD/3.png",
-"https://i.ibb.co/yRmsMWW/4.png",
-"https://i.ibb.co/ysVYxV9/5.png"
+       "https://i.ibb.co/k2Tj2Fx/1.png",
+"https://i.ibb.co/2PqXp2Q/2.png",
+"https://i.ibb.co/tHfKvBB/3.png",
+"https://i.ibb.co/qCqtjvL/4.png",
+"https://i.ibb.co/yPh3rJ2/5.png"
     ];
 
 function mySlide(param) {
